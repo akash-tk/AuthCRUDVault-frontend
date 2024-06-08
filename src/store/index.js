834d15a8262e493d9f4cc342be3eedac72dab8ae
Akash T K <akash.tk333@gmail.com>
2024-06-08 20:57:33 +0530

    Delete src directory

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import { thunk } from 'redux-thunk';
-import rootReducer from './reducers';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
-export default store;
